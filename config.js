e877e4dff63f45f4dedde93b8aeabb0bc316f4c2
esfandani <esfandani@gmail.com>
2019-09-04 09:28:49 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+{
+    "uswm": {
+      "on_handle": "uswm.visit.{{var}}.all",
+      "fmVisit": "(uswm.visit.fm.{{var}})",
+      "assignedVisit": "(uswm.visit.assigned.{{var}})",
+      "visitConversion": "uswm.visit.fm.{{var}}.thank_you_view_thankyou.beta/uswm.visit.fm.{{var}}"
+    }
+}
